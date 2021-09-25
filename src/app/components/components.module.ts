2b1bc1bd81b440e66465f8cebc517432f47984ef
patriciomardonesduoc <82007366+patriciomardonesduoc@users.noreply.github.com>
2021-09-25 12:54:39 -0300

    Agrega component a modulo

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    EncabezadoComponent
   ]
 })
 export class ComponentsModule { }
