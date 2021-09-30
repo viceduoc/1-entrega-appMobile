07135be897aded1b0de6850722dfc9b5d856d715
patriciomardonesduoc <82007366+patriciomardonesduoc@users.noreply.github.com>
2021-09-29 21:22:24 -0300

    se agrega logica a pswd-recovery

--- src/app/pages/pswd-recovery/pswd-recovery.page.ts
+++ src/app/pages/pswd-recovery/pswd-recovery.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pswd-recovery',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PswdRecoveryPage implements OnInit {
 
-  constructor() { }
+  user:any = {
+    username : "",
+    password1 : "",
+    password2 : ""
+  };
+
+  constructor(private activatedRoute:ActivatedRoute, private router:Router) {
+    this.activatedRoute.queryParams.subscribe(
+        params => {
+          if (this.router.getCurrentNavigation().extras.state){
+            this.user.username = this.router.getCurrentNavigation().extras.state.user.username;
+          }
+        }
+    )
+
+  };
+
+  validaPassword(){
+    if (this.user.password1 == this.user.password2 && this.user.password1.length > 0 && this.user.username.length > 0){
+      console.log('Passwords validas. Redirigiendo...')
+
+      this.router.navigate(['login'])
+    }
+    else {
+      console.log("No coinciden.")
+    }
+  }
 
   ngOnInit() {
   }
